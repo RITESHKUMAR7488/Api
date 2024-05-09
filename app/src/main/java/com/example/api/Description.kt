5e0f3d77d96dbf50184bbd4052cdcb15c5c8b97f
RITESHKUMAR7488 <riteshman30@gmail.com>
2024-05-09 11:35:24 +0530

    first commit66

---
+++ app/src/main/java/com/example/api/Description.kt
@@ -0,0 +1,29 @@
+package com.example.api
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.api.databinding.ActivityDescriptionBinding
+import com.squareup.picasso.Picasso
+
+class Description : AppCompatActivity() {
+    lateinit var binding: ActivityDescriptionBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=DataBindingUtil.setContentView(this@Description,R.layout.activity_description)
+        with(binding){
+            val title=intent.getStringExtra("title")
+            val description=intent.getStringExtra("description")
+            val imageUrl=intent.getStringExtra("image_url")
+
+            dpProductTitle.text=title
+            productDes.text=description
+            Picasso.get().load(imageUrl).into(dpProductPic)
+        }
+
+    }
+}
